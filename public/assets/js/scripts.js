5de3dc84af979800a51851a010413833b1a15e61
Wassim <wbacha95@gmail.com>
2023-04-03 17:03:30 +0200

    xD

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,1 +1,17 @@
 console.log("Bonjour");
+
+function Action1(){
+    Q1.classList.remove("question-form");
+    Q1.classList.add("question-form-invisible");
+    Q2.classList.remove("question-form-invisible");
+    Q2.classList.add("question-form");
+    console.log("1");
+}
+
+function Action2(){
+    Q2.classList.remove("question-form");
+    Q2.classList.add("question-form-invisible");
+    Q3.classList.remove("question-form-invisible");
+    Q3.classList.add("question-form");
+    console.log("1");
+}
