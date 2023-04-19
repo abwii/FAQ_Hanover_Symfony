3c1cf534843298bc28af6374a9291b334e130fe6
Wassim <66485319+abwii@users.noreply.github.com>
2023-04-19 14:21:45 +0200

    test yacine

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -30,3 +30,5 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
+
+//salut c'est un text d'exemple
