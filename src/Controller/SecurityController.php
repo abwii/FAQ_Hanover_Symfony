f844666db886e1260bec2aac8445b904ee1a2906
Wassim <66485319+abwii@users.noreply.github.com>
2023-04-19 14:24:47 +0200

    enft non
    c'était un test, faites pas gaffe

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -30,5 +30,3 @@ class SecurityController extends AbstractController
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
-
-//salut c'est un text d'exemple
